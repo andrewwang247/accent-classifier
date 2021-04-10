9b1048ab5c0c57d29fd2c1209858370efb095b0e
Siwei Wang <siweiw9@gmail.com>
2021-04-09 23:52:24 -0400

    elastic regularization

--- bilstm.py
+++ bilstm.py
@@ -3,26 +3,28 @@ Bidirectional LSTM model.
 
 Copyright 2021. Siwei Wang.
 """
-from typing import List, Tuple
-from tensorflow.keras import layers  # type: ignore
+from typing import Tuple
+from tensorflow.keras import layers, regularizers  # type: ignore
 from tensorflow.keras import Model, Sequential
 
 
-def _get_bilstm_layers(in_shape: Tuple[int, ...],
-                       num_labels: int) -> List[layers.Layer]:
-    return [
-        layers.Reshape((-1, 1), input_shape=in_shape),
-        layers.Bidirectional(
-            layers.LSTM(20, return_sequences=True)),
-        layers.Dropout(0.25),
-        layers.Bidirectional(layers.LSTM(20)),
-        layers.Dropout(0.25),
-        layers.Dense(num_labels, activation='softmax')]
+def _get_bilstm_layer(units: int, ret_seq: bool) -> layers.Layer:
+    """Create l1_l2 regularized BiLSTM layer."""
+    return layers.Bidirectional(
+        layers.LSTM(units, return_sequences=ret_seq,
+                    kernel_regularizer=regularizers.l1_l2(),
+                    recurrent_regularizer=regularizers.l1_l2())
+    )
 
 
 def get_bilstm(in_shape: Tuple[int, ...], num_labels: int) -> Model:
-    """Retrieve BiLSTM model."""
+    """Define and retrieve BiLSTM model."""
     model = Sequential(
-        layers=_get_bilstm_layers(in_shape, num_labels),
+        layers=[layers.Reshape((-1, 1), input_shape=in_shape),
+                _get_bilstm_layer(10, True),
+                layers.Dropout(0.25),
+                _get_bilstm_layer(10, False),
+                layers.Dropout(0.25),
+                layers.Dense(num_labels, activation='softmax')],
         name='BiLSTM')
     return model
