035b0aa0474ee215a9acb82a7d2f2c8324dc255e
Siwei Wang <siweiw9@gmail.com>
2021-04-09 21:05:41 -0400

    removed mel, improved pipeline

--- bilstm.py
+++ bilstm.py
@@ -5,15 +5,15 @@ Copyright 2021. Siwei Wang.
 """
 from typing import List, Tuple
 from tensorflow.keras import layers  # type: ignore
-from tensorflow.keras import Model, Sequential  # type: ignore
+from tensorflow.keras import Model, Sequential
 
 
 def _get_bilstm_layers(in_shape: Tuple[int, ...],
                        num_labels: int) -> List[layers.Layer]:
     return [
+        layers.Reshape((-1, 1), input_shape=in_shape),
         layers.Bidirectional(
-            layers.LSTM(20, return_sequences=True,
-                        input_shape=(None, *in_shape))),
+            layers.LSTM(20, return_sequences=True)),
         layers.Bidirectional(
             layers.LSTM(20, return_sequences=True)),
         layers.Bidirectional(
--- preprocess.py
+++ preprocess.py
@@ -3,40 +3,29 @@ Load and preprocess recordings.
 
 Copyright 2021. Siwei Wang.
 """
-from typing import Dict, Iterable, Tuple, Union
+from typing import Dict, Tuple, Union
 from os import path, scandir
-from functools import partial
-import numpy as np  # type: ignore
-from librosa.util import normalize  # type: ignore
-from librosa.feature import melspectrogram  # type: ignore
 import tensorflow as tf  # type: ignore
 # pylint: disable=redefined-outer-name
 
 DATA_DIR = 'recordings'
 ACCENTS = sorted(acc.name for acc in scandir(DATA_DIR))
-# The output mel shape depends on librosa default parameters
-MEL_SHAPE = (40, 128)
 
 
 def file_split(accent: str, hyp: Dict[str, Union[float, int]]) \
-        -> Tuple[tf.data.Dataset, tf.data.Dataset]:
-    """Load filenames into datasets and split into train and test."""
+        -> Tuple[tf.data.Dataset, tf.data.Dataset, tf.data.Dataset]:
+    """Load filenames into datasets and split into train, val, test."""
     acc_dir = path.join(DATA_DIR, accent)
     acc_glob = path.join(acc_dir, '*.wav')
     file_list = tf.data.Dataset.list_files(acc_glob)
-    num_files = sum(1 for _ in scandir(acc_dir))
+    num_files = sum(1 for _ in file_list)
     num_test = round(hyp['test_split'] * num_files)
-    return file_list.skip(num_test), file_list.take(num_test)
-
-
-def mel_generator(data: tf.data.Dataset,
-                  hyp: Dict[str, Union[float, int]]) -> Iterable[tf.Tensor]:
-    """Construct mel spec generator for given dataset."""
-    for audio in data:
-        spec = melspectrogram(np.array(audio), sr=hyp['sampling_rate'])
-        mel_log = normalize(np.log(spec + 1e-9))
-        # transpose to get time on axis 0
-        yield tf.convert_to_tensor(mel_log.T, dtype=tf.float32)
+    num_val = round(hyp['val_split'] * num_files)
+    test = file_list.take(num_test)
+    train_val = file_list.skip(num_test)
+    val = train_val.take(num_val)
+    train = train_val.skip(num_val)
+    return train, val, test
 
 
 def transform_files(filenames: tf.data.Dataset,
@@ -52,24 +41,22 @@ def transform_files(filenames: tf.data.Dataset,
         .interleave(tf.data.Dataset.from_tensor_slices,
                     cycle_length=tf.data.AUTOTUNE,
                     num_parallel_calls=tf.data.AUTOTUNE)
-    specs = tf.data.Dataset.from_generator(
-        partial(mel_generator, audio, hyp),
-        output_signature=tf.TensorSpec(shape=MEL_SHAPE))
-    lbl = [0.0] * len(ACCENTS)
-    lbl[label] = 1.0
-    const = tf.data.Dataset.from_tensors(lbl).repeat()
-    return tf.data.Dataset.zip((specs, const))
+    const = tf.data.Dataset.from_tensors([label]).repeat()
+    return tf.data.Dataset.zip((audio, const))
 
 
 def load_accents(hyp: Dict[str, Union[float, int]]) \
-        -> Tuple[tf.data.Dataset, tf.data.Dataset]:
+        -> Tuple[tf.data.Dataset, tf.data.Dataset, tf.data.Dataset]:
     """Load all accents into datasets."""
-    train, test = [transform_files(ds, 0, hyp)
-                   for ds in file_split(ACCENTS[0], hyp)]
+    train, val, test = [transform_files(ds, 0, hyp)
+                        for ds in file_split(ACCENTS[0], hyp)]
     for lbl, accent in enumerate(ACCENTS[1:], start=1):
-        current_train, current_test = [transform_files(ds, lbl, hyp)
-                                       for ds in file_split(accent, hyp)]
-        train = train.concatenate(current_train)
-        test = test.concatenate(current_test)
-    return train.shuffle(hyp['shuffle_buffer']), \
-        test.shuffle(hyp['shuffle_buffer'])
+        new_train, new_val, new_test = [transform_files(ds, lbl, hyp)
+                                        for ds in file_split(accent, hyp)]
+        train = train.concatenate(new_train)
+        val = val.concatenate(new_val)
+        test = test.concatenate(new_test)
+    buff = hyp['shuffle_buffer']
+    return train.shuffle(buff), \
+        val.shuffle(buff), \
+        test.shuffle(buff)
