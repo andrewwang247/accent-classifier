4f7b16ba1d52fd43df85eea67f728441c28a4190
Siwei Wang <siweiw9@gmail.com>
2021-04-09 21:05:41 -0400

    removed mel, improved pipeline

--- train.py
+++ train.py
@@ -3,37 +3,68 @@ Train models on data.
 
 Copyright 2021. Siwei Wang.
 """
-from typing import Dict, Union
+from typing import Dict, List, Union
 from json import load
 from tensorflow import keras  # type: ignore
-from preprocess import ACCENTS, load_accents, MEL_SHAPE
+from matplotlib import pyplot as plt  # type: ignore
+from preprocess import ACCENTS, load_accents
 from bilstm import get_bilstm
 # pylint: disable=redefined-outer-name
 
 HYP_FILE = 'hyperparameters.json'
+# Total number of signals in entire dataset
+APPROX_TOTAL_SIGS = 240_000_000
+
+
+def epoch_steps(source: str, hyp: Dict[str, Union[float, int]]) -> int:
+    """Approximate epoch steps to cover the dataset."""
+    assert source in ('train', 'val', 'test')
+    frac = 1 - hyp['val_split'] - hyp['test_split'] \
+        if source == 'train' else hyp[f'{source}_split']
+    windows = frac * APPROX_TOTAL_SIGS / hyp['frame_step']
+    return round(windows / hyp['batch_size'])
+
+
+def plot_history(history: Dict[str, List[float]], fpath: str):
+    """Save a matplotlib plot of training history."""
+    plt.figure(dpi=1200)
+    for points in history.values():
+        plt.plot(points)
+    plt.legend(history.keys())
+    plt.xlabel('Epoch')
+    plt.title('Training Plot')
+    plt.savefig(fpath)
 
 
 def train():
     """Train models on data."""
-    with open(HYP_FILE) as fp_hyp:
-        hyp: Dict[str, Union[float, int]] = load(fp_hyp)
+    with open(HYP_FILE) as fin:
+        hyp: Dict[str, Union[float, int]] = load(fin)
     batch_size = hyp['batch_size']
-    train, test = [ds.repeat().batch(batch_size)
-                   for ds in load_accents(hyp)]
+    train, val, test = [ds.repeat()
+                        .batch(batch_size, drop_remainder=True)
+                        .prefetch(1)
+                        for ds in load_accents(hyp)]
 
-    model = get_bilstm(MEL_SHAPE, len(ACCENTS))
-    model.build((batch_size, *MEL_SHAPE))
+    in_shape = (hyp['frame_len'],)
+    model = get_bilstm(in_shape, len(ACCENTS))
+    model.build((batch_size, *in_shape))
     model.compile(optimizer=keras.optimizers.Adam(hyp['learning_rate']),
-                  loss='categorical_crossentropy',
-                  metrics=['categorical_accuracy'])
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['sparse_categorical_accuracy'])
     model.summary()
 
     checkpoint = keras.callbacks.ModelCheckpoint('saved/bilstm_{epoch:02d}.h5')
-    model.fit(train, batch_size=batch_size,
-              epochs=hyp['epochs'], callbacks=[checkpoint],
-              steps_per_epoch=hyp['steps_per_epoch'],
-              workers=12, use_multiprocessing=True)
-    model.evaluate(test, batch_size=batch_size)
+    hist = model.fit(train, batch_size=batch_size,
+                     steps_per_epoch=epoch_steps('train', hyp),
+                     epochs=hyp['epochs'], callbacks=[checkpoint],
+                     validation_data=val, validation_batch_size=batch_size,
+                     validation_steps=epoch_steps('val', hyp),
+                     workers=12, use_multiprocessing=True)
+    result = model.evaluate(test, batch_size=batch_size,
+                            steps=epoch_steps('test', hyp))
+    plot_history(hist.history, 'train_plot.png')
+    print(f'Result: {result}')
 
 
 if __name__ == '__main__':
