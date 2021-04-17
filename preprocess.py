f9b0cbbfa6879482eb811e7e84f7640ecf5593a2
Siwei Wang <siweiw9@gmail.com>
2021-04-17 04:00:47 -0400

    silence trimming, doesn't work yet

--- preprocess.py
+++ preprocess.py
@@ -3,20 +3,22 @@ Load and preprocess recordings.
 
 Copyright 2021. Siwei Wang.
 """
-from typing import Dict, List, Tuple, Union
+from typing import Dict, Tuple, Union
 from os import path
-import numpy as np  # type: ignore
+from functools import partial
 import tensorflow as tf  # type: ignore
 import tensorflow_io as tfio  # type: ignore
+from pydub import AudioSegment  # type: ignore
+from pydub.silence import split_on_silence  # type: ignore
 from util import DATA_DIR, ACCENTS
+from util import _standardize_tensor, _normalize_tensor
 # pylint: disable=redefined-outer-name
 
 
 def _file_split(accent: str, hyp: Dict[str, Union[float, int]]) \
         -> Tuple[tf.data.Dataset, tf.data.Dataset, tf.data.Dataset]:
     """Load filenames into datasets and split into train, val, test."""
-    acc_dir = path.join(DATA_DIR, accent)
-    acc_glob = path.join(acc_dir, '*.wav')
+    acc_glob = path.join(DATA_DIR, accent, '*.wav')
     file_list = tf.data.Dataset.list_files(acc_glob)
     num_files = sum(1 for _ in file_list)
     num_test = round(hyp['test_split'] * num_files)
@@ -28,49 +30,34 @@ def _file_split(accent: str, hyp: Dict[str, Union[float, int]]) \
     return train, val, test
 
 
-def _standardize_tensor(tens: tf.Tensor) -> tf.Tensor:
-    """Rescale tensor to 0 mean and 1 std."""
-    mean = tf.reduce_mean(tens)
-    std = tf.math.reduce_std(tens)
-    denom = std if std != 0.0 else tf.constant(1e-4)
-    return (tens - mean) / denom
-
-
-def _decode_wav_files(filenames: tf.data.Dataset) -> tf.data.Dataset:
-    """Read, decode, and reshape filenames dataset."""
-    return filenames.map(tf.io.read_file) \
-        .map(tf.audio.decode_wav) \
-        .map(lambda amp: tf.reshape(amp[0], [-1]))
-
-
-def dataset_class_weights(accents: List[str]) -> Dict[int, float]:
-    """Compute class weights on imbalanced accents."""
-    samples = np.empty(len(accents), dtype=int)
-    for idx, accent in enumerate(accents):
-        glb = path.join(DATA_DIR, accent, '*.wav')
-        filenames = tf.data.Dataset.list_files(glb)
-        audio = _decode_wav_files(filenames)
-        samples[idx] = sum(aud.shape[0] for aud in audio)
-    weights = np.sum(samples) / samples / len(accents)
-    return dict(enumerate(weights / np.sum(weights)))
-
-
-def _frame_audio(audio: tf.data.Dataset,
-                 hyp: Dict[str, Union[float, int]]) -> tf.data.Dataset:
-    """Split audio into frames."""
-    return audio.map(lambda sig: tf.signal.frame(sig, hyp['frame_len'],
-                                                 hyp['frame_step'])) \
-        .interleave(tf.data.Dataset.from_tensor_slices,
-                    cycle_length=tf.data.AUTOTUNE,
-                    num_parallel_calls=tf.data.AUTOTUNE)
+def _remove_silence(fpath: tf.Tensor,
+                    hyp: Dict[str, Union[float, int]]) \
+        -> tf.Tensor:
+    """Load and remove silence silence from audio file."""
+    audio = AudioSegment.from_wav(bytes.decode(fpath.numpy(), 'utf-8'))
+    chunks = split_on_silence(audio,
+                              min_silence_len=hyp['min_silence_len'],
+                              silence_thresh=hyp['silence_thresh'])
+    combined = sum(chunks)
+    assert isinstance(combined, AudioSegment)
+    sample = combined.get_array_of_samples()
+    return tf.convert_to_tensor(sample, dtype=tf.int16)
 
 
 def _transform_files(filenames: tf.data.Dataset,
                      label: int,
                      hyp: Dict[str, Union[float, int]]) -> tf.data.Dataset:
     """Transform filenames of the same label to labelled dataset."""
-    audio = _decode_wav_files(filenames)
-    frames = _frame_audio(audio, hyp)
+    clean_up = partial(_remove_silence, hyp=hyp)
+    py_func = tf.autograph.experimental.do_not_convert(
+        lambda fpath: tf.py_function(clean_up, [fpath], tf.int16))
+    frames = filenames.map(py_func) \
+        .map(_normalize_tensor) \
+        .map(lambda sig: tf.signal.frame(sig, hyp['frame_len'],
+                                         hyp['frame_step'])) \
+        .interleave(tf.data.Dataset.from_tensor_slices,
+                    cycle_length=tf.data.AUTOTUNE,
+                    num_parallel_calls=tf.data.AUTOTUNE)
     specs = frames.map(lambda sig: tfio.experimental.audio.
                        spectrogram(sig, hyp['num_fft'],
                                    hyp['spec_window'],
