d92277da2c6b7bbeff439d3350ace823a6a9b7f2
Siwei Wang <siweiw9@gmail.com>
2021-04-17 16:31:36 -0400

    more complex model for larger input

--- model.py
+++ model.py
@@ -6,13 +6,13 @@ Copyright 2021. Siwei Wang.
 from typing import List, Tuple
 import tensorflow as tf  # type: ignore
 from tensorflow.keras import Model, Sequential, layers  # type: ignore
-from tensorflow.keras.regularizers import l1_l2, Regularizer  # type: ignore
+from tensorflow.keras.regularizers import L2, Regularizer  # type: ignore
 from tensorflow.keras.initializers import LecunNormal  # type: ignore
 
 
 def _regularizer() -> Regularizer:
     """Create a regularizer."""
-    return l1_l2(5e-3, 1e-2)
+    return L2(1e-4)
 
 
 def _conv_layer(filters: int, kernel_sz: int) -> layers.Conv2D:
@@ -20,8 +20,7 @@ def _conv_layer(filters: int, kernel_sz: int) -> layers.Conv2D:
     return layers.Conv2D(filters, kernel_sz, padding='same',
                          activation='selu',
                          kernel_initializer=LecunNormal(),
-                         kernel_regularizer=_regularizer(),
-                         activity_regularizer=_regularizer())
+                         kernel_regularizer=_regularizer())
 
 
 def _lstm_layer(units: int, ret_seq: bool) -> layers.LSTM:
@@ -29,8 +28,7 @@ def _lstm_layer(units: int, ret_seq: bool) -> layers.LSTM:
     return layers.LSTM(units, return_sequences=ret_seq,
                        kernel_regularizer=_regularizer(),
                        recurrent_regularizer=_regularizer(),
-                       activity_regularizer=_regularizer(),
-                       dropout=0.2, recurrent_dropout=0.4)
+                       dropout=0.2, recurrent_dropout=0.2)
 
 
 def _global_depth_pool(pool_op: str) -> layers.Lambda:
@@ -44,20 +42,20 @@ def _global_depth_pool(pool_op: str) -> layers.Lambda:
 def _cnn_layers(in_shape: Tuple[int, ...]) -> List[layers.Layer]:
     """Get a list of layers for CNN without final predictor."""
     return [layers.Reshape((*in_shape, 1), input_shape=in_shape),
-            _conv_layer(32, 5),
-            layers.MaxPool2D(pool_size=(1, 2)),
-            layers.Dropout(0.3),
-            _conv_layer(64, 3),
-            layers.MaxPool2D(pool_size=(1, 2)),
-            layers.Dropout(0.3),
-            _global_depth_pool('max')]
+            _conv_layer(64, 5),
+            layers.MaxPool2D(pool_size=(2, 2)),
+            layers.Dropout(0.2),
+            _conv_layer(128, 3),
+            layers.MaxPool2D(pool_size=(2, 2)),
+            layers.Dropout(0.2),
+            _global_depth_pool('avg')]
 
 
 def _lstm_layers(num_labels: int) -> List[layers.Layer]:
     """Get a list of layers for LSTM with final predictor."""
-    return [layers.Bidirectional(_lstm_layer(16, True)),
-            layers.Bidirectional(_lstm_layer(16, True)),
-            layers.Bidirectional(_lstm_layer(16, False)),
+    return [layers.Bidirectional(_lstm_layer(48, True)),
+            layers.Bidirectional(_lstm_layer(48, True)),
+            layers.Bidirectional(_lstm_layer(48, False)),
             layers.Dense(num_labels, activation='softmax')]
 
 
