46be90d7bf509f864325ac00f2b12d6935a8c7a1
Siwei Wang <siweiw9@gmail.com>
2021-04-18 22:46:51 -0400

    drastic reduction of model capacity, dbscale spectrograms

--- train.py
+++ train.py
@@ -12,24 +12,8 @@ from tensorflow.keras import optimizers, callbacks  # type: ignore
 from click import command, option  # type: ignore
 from preprocess import load_accents
 from util import ACCENTS, ARTIFACT_DIR, DATA_DIR
-from util import hyperparameters, data_shape, get_model
+from util import hyperparams, data_shape, get_model, compute_steps
 # pylint: disable=redefined-outer-name,no-value-for-parameter
-
-
-def plot_history(history: Dict[str, List[int]],
-                 metrics: List[str], model_name: str, dpi: int):
-    """Plot training and validation metrics and loss."""
-    for metric in metrics + ['loss']:
-        plt.figure(dpi=dpi)
-        plt.plot(history[metric])
-        plt.plot(history[f'val_{metric}'])
-        plt.legend([metric, f'val_{metric}'])
-        plt.xlabel('Epoch')
-        plt.ylabel(f'{metric.capitalize()} Value')
-        plt.title(f'{metric.capitalize()} over Training')
-        plt.savefig(path.join(ARTIFACT_DIR,
-                              f'{model_name}_{metric}.png'))
-        plt.close()
 
 
 def compute_class_weights(accents: List[str]) -> Dict[int, float]:
@@ -42,12 +26,37 @@ def compute_class_weights(accents: List[str]) -> Dict[int, float]:
     return dict(enumerate(weights / np.sum(weights)))
 
 
+def dump_history(hist: Dict[str, List[float]],
+                 model_name: str):
+    """Save history from training."""
+    hist_path = path.join(ARTIFACT_DIR, f'{model_name}_history.json')
+    with open(hist_path, 'w') as fp_out:
+        dump(hist, fp_out, indent=2)
+
+
+def plot_history(history: Dict[str, List[int]],
+                 metrics: List[str], model_name: str):
+    """Plot training and validation metrics and loss."""
+    for metric in metrics + ['loss']:
+        plt.figure()
+        plt.plot(history[metric])
+        plt.plot(history[f'val_{metric}'])
+        plt.legend([metric, f'val_{metric}'])
+        plt.xlabel('Epoch')
+        plt.ylabel(f'{metric.capitalize()} Value')
+        plt.title(f'{metric.capitalize()} over Training')
+        plt.savefig(path.join(ARTIFACT_DIR,
+                              f'{model_name}_{metric}.png'))
+        plt.close()
+
+
 @command()
 @option('--cnn', '-c', is_flag=True,
         help='Set flag to train CNN-BiLSTM.')
 def train(cnn: bool):
     """Train model on data. Evaluate test set on best models."""
-    hyp = hyperparameters()
+    hyp = hyperparams()
+    plt.rcParams['figure.dpi'] = hyp['plot_dpi']
     train, val, test = [ds.repeat().shuffle(hyp['shuffle_buffer'])
                         .batch(hyp['batch_size'], drop_remainder=True)
                         .prefetch(1)
@@ -67,31 +76,28 @@ def train(cnn: bool):
         monitor=f'val_{met}', save_best_only=True)
         for met in tracked_metrics + ['loss']]
     weights = compute_class_weights(ACCENTS)
+    train_steps, val_steps, test_steps = compute_steps(hyp)
     try:
         hist = model.fit(
             train,
             epochs=hyp['epochs'],
             class_weight=weights,
-            steps_per_epoch=hyp['train_steps'],
+            steps_per_epoch=train_steps,
             callbacks=checkpoints,
             validation_data=val,
-            validation_steps=hyp['val_steps'],
+            validation_steps=val_steps,
             workers=hyp['cpu_cores'],
             use_multiprocessing=True)
 
         assert hist is not None
-        hist_path = path.join(ARTIFACT_DIR, f'{model.name}_history.json')
-        with open(hist_path, 'w') as fp_out:
-            dump(hist.history, fp_out, indent=2)
-        dpi = hyp['plot_dpi']
-        assert isinstance(dpi, int)
-        plot_history(hist.history, tracked_metrics, model.name, dpi)
+        dump_history(hist.history, model.name)
+        plot_history(hist.history, tracked_metrics, model.name)
     finally:
         for met in tracked_metrics + ['loss']:
             print(f'Evaluating {model.name} with best {met}...')
             model.load_weights(
                 path.join(ARTIFACT_DIR, f'{model.name}_{met}.hdf5'))
-            model.evaluate(test, steps=hyp['test_steps'])
+            model.evaluate(test, steps=test_steps)
 
 
 if __name__ == '__main__':
--- visualize.py
+++ visualize.py
@@ -8,7 +8,7 @@ from matplotlib import pyplot as plt  # type: ignore
 from seaborn import heatmap  # type: ignore
 from click import command, option  # type: ignore
 import tensorflow as tf  # type: ignore
-from util import ARTIFACT_DIR, hyperparameters
+from util import ARTIFACT_DIR, hyperparams
 from preprocess import load_accents
 # pylint: disable=no-value-for-parameter
 
@@ -18,17 +18,16 @@ from preprocess import load_accents
         help='The number of plots to create.')
 def main(num: int):
     """Visualize mel spectrogram input data."""
-    hyp = hyperparameters()
+    hyp = hyperparams()
     train, val, test = load_accents(hyp)
     dataset = train.concatenate(val) \
         .concatenate(test) \
         .shuffle(hyp['shuffle_buffer']) \
         .take(num).prefetch(1)
-    plot_dpi = hyp['plot_dpi']
-    assert isinstance(plot_dpi, int)
+    plt.rcParams['figure.dpi'] = hyp['plot_dpi']
     for idx, (spec, _) in enumerate(dataset):
         fpath = path.join(ARTIFACT_DIR, f'spec_{idx:02d}.png')
-        plt.figure(dpi=plot_dpi)
+        plt.figure()
         heatmap(tf.transpose(spec))
         plt.savefig(fpath)
         plt.close()
