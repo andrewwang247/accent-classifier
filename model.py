133d35f3841b194b6a555522a879ee100ac9e8c8
Siwei Wang <siweiw9@gmail.com>
2021-04-19 04:50:55 -0400

    skinnier, deeper model

--- model.py
+++ model.py
@@ -28,7 +28,7 @@ def _lstm_layer(units: int, ret_seq: bool) -> layers.LSTM:
     return layers.LSTM(units, return_sequences=ret_seq,
                        kernel_regularizer=_regularizer(),
                        recurrent_regularizer=_regularizer(),
-                       recurrent_dropout=0.4)
+                       recurrent_dropout=0.3)
 
 
 def _global_depth_pool(pool_op: str) -> layers.Lambda:
@@ -42,11 +42,13 @@ def _global_depth_pool(pool_op: str) -> layers.Lambda:
 def _cnn_layers(in_shape: Tuple[int, ...]) -> List[layers.Layer]:
     """Get a list of layers for CNN without final predictor."""
     return [layers.Reshape((*in_shape, 1), input_shape=in_shape),
-            _conv_layer(8, 5),
+            _conv_layer(6, 5),
             layers.MaxPool2D(pool_size=(2, 2)),
             layers.Dropout(0.3),
-            _conv_layer(16, 3),
+            _conv_layer(9, 3),
             layers.MaxPool2D(pool_size=(2, 2)),
+            layers.Dropout(0.3),
+            _conv_layer(12, 3),
             layers.Dropout(0.3),
             _global_depth_pool('max')]
 
@@ -54,7 +56,8 @@ def _cnn_layers(in_shape: Tuple[int, ...]) -> List[layers.Layer]:
 def _lstm_layers(num_labels: int) -> List[layers.Layer]:
     """Get a list of layers for LSTM with final predictor."""
     return [layers.Bidirectional(_lstm_layer(4, True)),
-            layers.Bidirectional(_lstm_layer(8, False)),
+            layers.Bidirectional(_lstm_layer(6, True)),
+            layers.Bidirectional(_lstm_layer(6, False)),
             layers.Dense(num_labels, activation='softmax')]
 
 
